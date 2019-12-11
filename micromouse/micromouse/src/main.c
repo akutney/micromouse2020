fe01d515f000619b33f5fc522a131b1acc3fd2ef
Dylan Staatz <dms9800@gmail.com>
2019-12-10 20:15:08 -0600

    Fixed gitignore and added older clock

--- micromouse/micromouse/src/main.c
+++ micromouse/micromouse/src/main.c
@@ -27,7 +27,14 @@
 /*
  * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
  */
+
+#include "samd21.h"
 #include <asf.h>
+
+#include "clocks/ClocksInit.h"
+
+// Function Declarations
+void SysTickInit(void);
 
 int main (void)
 {
@@ -37,4 +44,31 @@ int main (void)
 	/* Insert application code here, after the board has been initialized. */
     printf("Hello World!\r\n");
     
+    ClocksInit();
+}
+
+void SysTick_Handler(void)
+{
+    static uint32_t tickCount = 0;
+
+    tickCount++ ;
+    // Toggle LEDs every second (i.e. 1000ms)
+    if(tickCount % 1000 == 0){
+        // Toggle LED pin output level.
+        PORT->Group[0].OUTTGL.reg |= 0x1 << 17;
+    }
+
+
+}
+
+
+void SysTickInit(void)
+{
+    // Configure SysTick to trigger an ISR every millisecond using a 48MHz CPU Clock
+    SysTick->CTRL = 0;					// Disable SysTick
+    SysTick->LOAD = 47999UL;			// Set reload register for 1mS interrupts
+    NVIC_SetPriority(SysTick_IRQn, 3);	// Set interrupt priority to least urgency
+    SysTick->VAL = 0;					// Reset the SysTick counter value
+    SysTick->CTRL = 0x00000007;			// Enable SysTick, Enable SysTick Exceptions, Use CPU Clock
+    NVIC_EnableIRQ(SysTick_IRQn);		// Enable SysTick Interrupt
 }
