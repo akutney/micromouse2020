fe10acf3781c7ff8a45c84e0504efe3dd1905492
Edward Gruver <edwardng21@gmail.com>
2020-02-20 20:03:22 -0600

    Added #include "i2c_driver.h"

--- software/src/drivers/led_driver.c
+++ software/src/drivers/led_driver.c
@@ -4,12 +4,14 @@
 
 #include <asf.h>
 #include <error.h>
+#include "i2c_driver.h"
 #include "../types/types.h"
 
 bool LED_DRIVER_ENABLED = true;
 
 int init_led_driver(void)
 {
+  init_i2c_driver();
   THROW_ERR("init_led_driver", ENOSYS);
 }
 
