b46a66754f4a718e606558157d301ef3fb7bf367
Edward Gruver <edwardng21@gmail.com>
2020-03-05 19:28:52 -0600

    Changed LED driver to allow for LED to turn on and off when communicating with I/O expander

--- software/src/drivers/led_driver.c
+++ software/src/drivers/led_driver.c
@@ -12,10 +12,19 @@ bool LED_DRIVER_ENABLED = true;
 int init_led_driver(void)
 {
   init_i2c_driver();
-  THROW_ERR("init_led_driver", ENOSYS);
+  write_reg16(IO_I2C_ADDRESS,0x06,0x0000);
 }
 
-int set_led_outputs(led_outputs_t * outputs)
+int set_led_outputs(led_outputs_t * out_led)
 {
+  uint16_t val = 0xFFFF;
+  
+  val = val & (out_led->outputs[LED0] ? ~0x0100 : ~0x0000);
+  val = val & (out_led->outputs[LED1] ? ~0x0200 : ~0x0000);
+  val = val & (out_led->outputs[LED2] ? ~0x0800 : ~0x0000);
+  val = val & (out_led->outputs[LED3] ? ~0x1000 : ~0x0000);
+  val = val & (out_led->outputs[LED4] ? ~0x0400 : ~0x0000);
+
+  write_reg16(IO_I2C_ADDRESS, 0x02, val);
   THROW_ERR("set_led_outputs", ENOSYS);
 }
